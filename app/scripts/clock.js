3c1292d64aad34b60c107ff374d972cf75af73e4
Vannio <singularity@vannio.co.uk>
2015-12-14 00:43:45 +0000

    Fixed clock refreshing

--- app/scripts/clock.js
+++ app/scripts/clock.js
@@ -1,14 +1,15 @@
 /*jshint unused:false*/
 
 var container = $('#js-current-time');
-var date = new Date(),
-		hour = ('0' + date.getHours()).slice(-2),
-		minutes = ('0' + date.getMinutes()).slice(-2),
-		seconds = ('0' + date.getSeconds()).slice(-2);
 
 module.exports = {
 	printTime: function() {
 		'use strict';
+
+		var date = new Date(),
+				hour = ('0' + date.getHours()).slice(-2),
+				minutes = ('0' + date.getMinutes()).slice(-2),
+				seconds = ('0' + date.getSeconds()).slice(-2);
 
 		container.html('<h1>' + hour + ' <span class="time-minutes">' + minutes + '</span><span class="time-seconds"> ' + seconds + '</span></h1>');
 	},
