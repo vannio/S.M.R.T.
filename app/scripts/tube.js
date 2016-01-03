e8be8da4bdba90b207b368672ff3017a0c91d2a9
Vannio <singularity@vannio.co.uk>
2016-01-03 00:10:54 +0000

    Colour + font changes

--- app/scripts/tube.js
+++ app/scripts/tube.js
@@ -22,19 +22,20 @@ module.exports = {
 	    },
 	    cache: false,
 	    printStatus: function(data) {
-
 	    	// Clear the container for updates
 	    	container.empty();
 
 	    	// Loop through set of data for each tube line..
 	    	for (var i = 0; i < data.length; i++){
 
+					var tubeStatus = data[i].lineStatuses[0].statusSeverityDescription;
+
 	    		// and check if the line matches one of the lines of interest..
 	    		if (lines.indexOf(data[i].id) > -1){
 
 	    			// if so, print the line status:
 						container.append(
-							'<li class="flex-container"><span class="tube-status">' + data[i].lineStatuses[0].statusSeverityDescription + ' on the </span><span class="tube-title tube-' + data[i].id + '">' + data[i].name + ' line</span></li>'
+							'<li class="flex-container"><span class="tube-status">' + tubeStatus.charAt(0).toUpperCase() + tubeStatus.slice(1).toLowerCase() + ' on the </span><span class="tube-title tube-' + data[i].id + '">' + data[i].name + ' line</span></li>'
 						);
 		    	}
 	    	}
